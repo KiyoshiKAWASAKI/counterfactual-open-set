282114701978d3122515d0cc1ff6e4020f6c55fb
Jin Huang <kiyoshi@Jins-MacBook-Pro-2019.local>
2022-06-07 16:33:13 -0400

    fix bugs in post-processing

--- generativeopenset/post_process.py
+++ generativeopenset/post_process.py
@@ -5,12 +5,7 @@ from scipy.special import softmax
 
 
 
-
-
-
-
-seed = 0
-
+seed = 2
 result_dir_base = "/afs/crc.nd.edu/user/j/jhuang24/scratch_50/" \
                   "jhuang24/models/osrci/seed_"
 
@@ -31,7 +26,6 @@ valid_label_path = result_dir_base + str(seed) + "/valid_labels.npy"
 valid_feature = np.load(valid_feature_path)
 valid_feature_aug = np.load(valid_feature_aug_path)
 valid_label = np.load(valid_label_path)
-
 
 # Test known output
 test_known_feature_p0_path = result_dir_base + str(seed) + "/test_p0_features.npy"
@@ -92,9 +86,28 @@ test_unknown_feature_aug = np.load(test_unknown_feature_aug_path)
 
 
 
-def get_known_results(original_feature,
-                      aug_feature,
-                      labels):
+def calculate_mcc(true_pos,
+                  true_neg,
+                  false_pos,
+                  false_neg):
+    """
+
+    :param true_pos:
+    :param true_neg:
+    :param false_pos:
+    :param false_negtive:
+    :return:
+    """
+
+    return (true_neg*true_pos-false_pos*false_neg)/np.sqrt((true_pos+false_pos)*(true_pos+false_neg)*
+                                                           (true_neg+false_pos)*(true_neg+false_neg))
+
+
+
+
+def get_train_valid_results(original_feature,
+                            aug_feature,
+                            labels):
     """
 
     :param original_feature:
@@ -106,8 +119,6 @@ def get_known_results(original_feature,
     wrong = 0
 
     true_positive = 0
-    true_negative = 0
-    false_positive = 0
     false_negative = 0
 
     print(original_feature[0].shape)
@@ -116,41 +127,32 @@ def get_known_results(original_feature,
         label = labels[i]
 
         max_prob = np.max(softmax(original_feature[i]))
-        pred = np.argmax(max_prob, axis=0)
+        pred = np.argmax(softmax(original_feature[i]), axis=0)
 
         prob_aug = softmax(aug_feature[i], axis=0)
         prob_unknown = prob_aug[-1]
 
-        if (max_prob-prob_unknown) >= 0.5:
+
+        if (prob_unknown-max_prob) >= 0.5:
+            wrong += 1
+
+        else:
             if pred == label:
                 correct += 1
-                true_positive += 1
             else:
                 wrong += 1
 
-        else:
-            wrong += 1
-            false_negative += 1
-
-    print("True positive: ", true_positive)
-    print("True negative: ", true_negative)
-    print("False postive: ", false_positive)
-    print("False negative: ", false_negative)
 
     accuracy = float(correct) / float(correct + wrong)
-    print("Accuracy: ", accuracy)
-
-
-    precision = float(true_positive) / float(true_positive + false_positive)
-    recall = float(true_positive) / float(true_positive + false_negative)
-    f1 = (2 * precision * recall) / (precision + recall)
-    print("F-1: ", f1)
+    print("Multi class accuracy: ", accuracy)
 
 
 
 
-def get_unknown_results(original_feature,
-                        aug_feature):
+def get_binary_results(original_known_feature,
+                       aug_known_feature,
+                       original_unknown_feature,
+                       aug_unknown_feature):
     """
 
     :param original_feature:
@@ -158,47 +160,67 @@ def get_unknown_results(original_feature,
     :param labels:
     :return:
     """
-    correct = 0
-    wrong = 0
+    true_positive = 0
+    true_negative = 0
+    false_positive = 0
+    false_negative = 0
 
+    # Process known samples
+    for i in range(len(original_known_feature)):
+        max_prob = np.max(softmax(original_known_feature[i], axis=0))
 
-    for i in range(len(original_feature)):
-        max_prob = np.max(softmax(original_feature[i], axis=0))
-
-        prob_aug = softmax(aug_feature[i], axis=0)
+        prob_aug = softmax(aug_known_feature[i], axis=0)
         prob_unknown = prob_aug[-1]
 
         if (prob_unknown-max_prob) >= 0.5:
-            correct += 1
+            false_negative += 1
         else:
-            wrong += 1
+            true_positive += 1
 
-    accuracy = float(correct) / float(correct + wrong)
-    print("Accuracy: ", accuracy)
+    # Process unknown
+    for i in range(len(original_unknown_feature)):
+        max_prob = np.max(softmax(original_unknown_feature[i], axis=0))
+
+        prob_aug = softmax(aug_unknown_feature[i], axis=0)
+        prob_unknown = prob_aug[-1]
+
+        if (prob_unknown-max_prob) >= 0.5:
+            true_negative += 1
+        else:
+            false_positive += 1
+
+    precision = float(true_positive) / float(true_positive + false_positive)
+    recall = float(true_positive) / float(true_positive + false_negative)
+    f1 = (2 * precision * recall) / (precision + recall)
+    mcc = calculate_mcc(true_pos=float(true_positive),
+                        true_neg=float(true_negative),
+                        false_pos=float(false_positive),
+                        false_neg=float(false_negative))
+    acc = float(true_negative)/float(true_negative+false_positive)
+
+    print("True positive: ", true_positive)
+    print("True negative: ", true_negative)
+    print("False postive: ", false_positive)
+    print("False negative: ", false_negative)
+    print("Unknown accuracy: ", acc)
+    print("F-1 score: ", f1)
+    print("MCC score: ", mcc)
+
 
 
 
 
 if __name__ == "__main__":
-    print("*" * 40)
-    print("Training results")
-    get_known_results(original_feature=train_feature,
-                      aug_feature=train_feature_aug,
-                      labels= train_label)
 
     print("*" * 40)
-    print("Validation results")
-    get_known_results(original_feature=valid_feature,
-                      aug_feature=valid_feature_aug,
-                      labels=valid_label)
+    print("Seed: ", seed)
+    print("Muti-class results")
+    get_train_valid_results(original_feature=test_known_feature,
+                            aug_feature=test_known_feature_aug,
+                            labels=test_known_labels)
 
-    print("*" * 40)
-    print("Test known results")
-    get_known_results(original_feature=test_known_feature,
-                      aug_feature=test_known_feature_aug,
-                      labels=test_known_labels)
-
-    print("*" * 40)
-    print("Test unknown results")
-    get_unknown_results(original_feature=test_unknown_feature,
-                      aug_feature=test_unknown_feature_aug)
+    print("Binary results")
+    get_binary_results(original_known_feature=test_known_feature,
+                       aug_known_feature=test_known_feature_aug,
+                       original_unknown_feature=test_unknown_feature,
+                       aug_unknown_feature=test_unknown_feature_aug)
